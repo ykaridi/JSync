4d0e75a1e202ad7307546ae814f5a7c733493065
ykaridi <ykaridi@gmail.com>
2024-07-23 15:52:00 +0300

    aesthetic changes

--- common/symbol.py
+++ common/symbol.py
@@ -9,8 +9,8 @@ SYMBOL_TYPE_CLASS = 2
 class Symbol(Dataclass):
     def __init__(self, symbol_type, canonical_signature, name, timestamp=None, author=None):
         # type: (int, str, str, int, str) -> None
-        self.author = author
-        self.symbol_type = symbol_type
-        self.canonical_signature = canonical_signature
-        self.name = name
-        self.timestamp = timestamp
+        self.symbol_type = symbol_type  # type: int
+        self.canonical_signature = canonical_signature  # type: str
+        self.name = name  # type: str
+        self.timestamp = timestamp  # type: int
+        self.author = author  # type: str
